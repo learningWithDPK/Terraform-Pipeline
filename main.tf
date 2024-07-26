cbcb35e867249bdf8c0aa4b72e72412ef31f808b
dpkkaushik88 <60708248+dpkkaushik88@users.noreply.github.com>
2024-07-26 13:05:38 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,65 +1,56 @@
 terraform {
-        backend "s3" {
-            bucket = "terraform-backend-05062024"
-            key ="terraform.tfstate"
-            region = "eu-north-1"
-        }
- }
+  backend "s3" {
+    bucket = "terraform-backend-05062024"
+    key    = "terraform.tfstate"
+    region = "eu-north-1"
+  }
+}
 
-
- terraform {
-# tflint failed as Required version was missing
-   required_version = ">= 1.0.9"      
-   required_providers {
-        aws = {
-            source= "hashicorp/aws"
-            version="5.50.0"
-        }
+terraform {
+  required_version = ">= 1.0.9"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.50.0"
     }
+  }
 }
 
 provider "aws" {
-    region = "eu-north-1"
-
+  region = "eu-north-1"
 }
 
 data "aws_ami" "ubuntu" {
-                most_recent = True
-        Filter {
-        name = name
-        values = ["ubuntu*"]
-        }
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu*"]
+  }
+  owners = ["self", "amazon"]
 }
+
 resource "aws_instance" "variable_testing" {
-   ami = data.aws_ami.ubuntu.id
-   instance_type = var.instance_type
-   availability_zone = "eu-north-1c"
-   root_block_device {
+  ami               = data.aws_ami.ubuntu.id
+  instance_type     = var.instance_type
+  availability_zone = "eu-north-1c"
+  root_block_device {
     volume_type = "gp2"
     volume_size = 20
     encrypted   = true
-  } 
-     tags = {
-       Name = "provisioner_testing"
- }
-metadata_options {
+  }
+  tags = {
+    Name = "provisioner_testing"
+  }
+  metadata_options {
     http_tokens = "required"
   }
 }
 
 variable "instance_type" {
-    type = string
-    default = "t3.micro"
+  type    = string
+  default = "t3.micro"
 }
 
-/*variable "ami" {
-    type = string
-    default = "ami-03238ca76a3266a07"
-}*/
-
-
 output "web_ip" {
-    value = aws_instance.variable_testing.public_ip
-  
-} 
- 
+  value = aws_instance.variable_testing.public_ip
+}
